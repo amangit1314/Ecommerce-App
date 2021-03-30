2bf8fb08ff82fd291b82cdc6081a8c2b9ed140ce
Aman Soni <65482419+gitaman8481@users.noreply.github.com>
2021-03-30 18:23:35 +0530

    models and utils done

---
+++ lib/main.dart
@@ -0,0 +1,4 @@
+import 'package:ecommerce_app/ecommerce_app.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(EcommerceApp());
---
+++ lib/utils/routes.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+final Map<String, WidgetBuilder> routes = {
+  SplashScreen.routeName: (context) => SplashScreen(),
+  SignInScreen.routeName: (context) => SignInScreen(),
+  ForgotPasswordScreen.routeName: (context) => ForgotPasswordScreen(),
+  LoginSuccessScreen.routeName: (context) => LoginSuccessScreen(),
+  SignUpScreen.routeName: (context) => SignUpScreen(),
+  CompleteProfileScreen.routeName: (context) => CompleteProfileScreen(),
+  OtpScreen.routeName: (context) => OtpScreen(),
+  HomeScreen.routeName: (context) => HomeScreen(),
+  DetailsScreen.routeName: (context) => DetailsScreen(),
+  CartScreen.routeName: (context) => CartScreen(),
+  ProfileScreen.routeName: (context) => ProfileScreen(),
+};
